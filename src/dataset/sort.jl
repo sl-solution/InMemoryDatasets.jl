42c52c6ff43ddd947cb51cebbe893f86ae1d11f6
sl-solution <github.sl.solution@icloud.com>
2021-06-15 17:20:27 +1200

    Update sort.jl

--- src/dataset/sort.jl
+++ src/dataset/sort.jl
@@ -1,8 +1,8 @@
-function Base.sort(ds::Dataset, cols::MultiColumnIndex; issorted = false)
+function Base.sort!(ds::Dataset, cols::MultiColumnIndex; issorted = false)
     colsidx = index(ds)[cols]
     if issorted
         selected_columns, ranges, last_valid_index = _find_starts_of_groups(ds::Dataset, colsidx, nrow(ds) < typemax(Int32) ? Val(Int32) : Val(Int64))
-        sds = SortedDataset(copy(ds), selected_columns, ranges, last_valid_index)
+        sds = SortedDataset(ds, selected_columns, ranges, last_valid_index)
         _modified(_attributes(sds))
         sds
     else
