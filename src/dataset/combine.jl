e3cac0003772c4f196ed9e57d20108c09a6d3ff8
sl-solution <github.sl.solution@icloud.com>
2021-06-25 19:40:46 +1200

    update the combine function

--- src/dataset/combine.jl
+++ src/dataset/combine.jl
@@ -1,26 +1,34 @@
 # first attemp for combine
 # Don't use it yet
-function combine(ds::Dataset, arg::Pair)
+function combine(ds::Dataset, @nospecialize(args...); makeunique = false)
     !isgrouped(ds) && throw(ArgumentError("`combine` is only for grouped data sets"))
-    idx_cpy = copy(index(ds))
-    ms = normalize_modify!(idx_cpy, index(ds), arg)
+    idx_cpy = Index(copy(index(ds).lookup), copy(index(ds).names), copy(index(ds).format))
+    ms = normalize_modify_multiple!(idx_cpy, index(ds), args...)
+    any(x->x.second.first isa Expr, ms) && throw(ArgumentError("`byrow` is not available for `combine`"))
     groupcols = index(ds).sortedcols
-    starts = group_starts(ds)
+    starts = index(ds).starts
     ngroups = index(ds).ngroups[]
 
     res = AbstractArray[]
     for j in 1:length(groupcols)
-        push!(res, ds[starts, groupcols[j]])
+        _tmpres = similar(ds[!, groupcols[j]], ngroups)
+        for i in 1:ngroups
+            _tmpres[i] = _columns(ds)[groupcols[j]][starts[i]]
+        end
+        push!(res, _tmpres)
     end
-    T = _check_the_output_type(ds, ms)
-    _res = Tables.allocatecolumn(T, ngroups)
-    for g in 1:ngroups
-        lo = starts[g]
-        g == ngroups ? hi = ngroups : hi = starts[g + 1] - 1
-        _res[g] = ms.second.first(view(_columns(ds)[ms.first], lo:hi))
-    end
-    push!(res, _res)
     nm = _names(ds)[groupcols]
-    push!(nm, ms.second.second)
-    Dataset(res, nm)
+    for i in 1:length(ms)
+        T = _check_the_output_type(ds, ms[i])
+        _res = Tables.allocatecolumn(T, ngroups)
+        # make sure lo and hi are not defined any where outside the following loops
+        Threads.@threads for g in 1:ngroups
+            lo = starts[g]
+            g == ngroups ? hi = nrow(ds) : hi = starts[g + 1] - 1
+            _res[g] = ms[i].second.first(view(_columns(ds)[ms[i].first], lo:hi))
+         end
+        push!(res, _res)
+        push!(nm, ms[i].second.second)
+    end
+    Dataset(res, nm, copycols = false; makeunique = makeunique)
 end
