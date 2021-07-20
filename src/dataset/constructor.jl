73be812b858a6bef833e4443cb1d9113729017e3
sl-solution <github.sl.solution@icloud.com>
2021-07-20 23:13:47 +1200

    bug fix

--- src/dataset/constructor.jl
+++ src/dataset/constructor.jl
@@ -214,13 +214,13 @@ function _preprocess_column(col::Any, len::Integer, copycols::Bool)
             return convert(Vector{Union{Bool, Missing}}, col)
         elseif eltype(col) <: Union{Missing, AbstractString} && !(eltype(col) <: Union{Missing, Characters})
             if DataAPI.refpool(col) !== nothing
-                ml = hp_maximum(length, DataAPI.refpool(col))
-                ul = hp_maximum(x->cld(sizeof(x), length(x)), DataAPI.refpool(col))
+                ml = hp_maximum(x->ismissing(x) ? 1 : length(x), DataAPI.refpool(col))
+                ul = hp_maximum(x->ismissing(x) ? 1 : cld(sizeof(x), length(x)), DataAPI.refpool(col))
                 ul == 1 ? col = map(x->Characters{ml, UInt8}(x), col) : col = map(x->Characters{ml, UInt16}(x), col)
                 return allowmissing(col)
             else
-                ml = hp_maximum(length, col)
-                ul = hp_maximum(x->cld(sizeof(x), length(x)), col)
+                ml = hp_maximum(x->ismissing(x) ? 1 : length(x), col)
+                ul = hp_maximum(x->ismissing(x) ? 1 : cld(sizeof(x), length(x)), col)
                 if ul == 1
                     return allowmissing(Characters{ml, UInt8}.(col))
                 else
