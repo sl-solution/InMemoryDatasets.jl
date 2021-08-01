1fce071576f4bb2b216022ed095204610222d82a
sl-solution <github.sl.solution@icloud.com>
2021-08-01 23:12:10 +1200

    improve performance

--- src/byrow/util.jl
+++ src/byrow/util.jl
@@ -1,11 +1,11 @@
 function mapreduce_index(f::Vector{<:Function}, op, itr, init)
     y = iterate(itr)
     y === nothing && return init
-    v = op(init, f[y[2][2]].(y[1]))
+    v = op(init, (y[1]), f[y[2][2]])
     while true
         y = iterate(itr, y[2])
         y === nothing && break
-        v = op(v, f[y[2][2]].(y[1]))
+        v = op(v, (y[1]), f[y[2][2]])
     end
     return v
 end
@@ -15,7 +15,7 @@ function row_any_multi(ds::AbstractDataset, f::Vector{<:Function}, cols = :)
     colsidx = index(ds)[cols]
     @assert length(f) == length(colsidx) "number of provided functions must match the number of selected columns"
     _op_bool_add_multi(x::Bool,y::Bool) = x | y ? true : false
-    op_for_any_multi!(x,y) = x .= _op_bool_add_multi.(x, y)
+    op_for_any_multi!(x, y, f) = x .= _op_bool_add_multi.(x, f.(y))
     # mapreduce(identity, op_for_anymissing!, eachcol(ds)[colsidx[sel_colsidx]], init = zeros(Bool, size(ds,1)))
     mapreduce_index(f, op_for_any_multi!, view(_columns(ds),colsidx), zeros(Bool, size(ds,1)))
 end
@@ -24,16 +24,16 @@ function row_all_multi(ds::AbstractDataset, f::Vector{<:Function}, cols = :)
     colsidx = index(ds)[cols]
     @assert length(f) == length(colsidx) "number of provided functions must match the number of selected columns"
     _op_bool_mult_multi(x::Bool,y::Bool) = x & y ? true : false
-    op_for_all_multi!(x,y) = x .= _op_bool_mult_multi.(x, y)
+    op_for_all_multi!(x, y, f) = x .= _op_bool_mult_multi.(x, f.(y))
     # mapreduce(identity, op_for_anymissing!, eachcol(ds)[colsidx[sel_colsidx]], init = zeros(Bool, size(ds,1)))
     mapreduce_index(f, op_for_all_multi!, view(_columns(ds),colsidx), ones(Bool, size(ds,1)))
 end
 
 _op_bool_add_multi(x::Bool,y::Bool) = x || y ? true : false
 
-function hp_bool_add_multi!(x, y)
+function hp_bool_add_multi!(x, y, f)
     Threads.@threads for i in 1:length(x)
-        @inbounds x[i] = _op_bool_add_multi(x[i], y[i])
+        @inbounds x[i] = _op_bool_add_multi(x[i], f(y[i]))
     end
     x
 end
@@ -41,16 +41,16 @@ end
 function hp_row_any_multi(ds::AbstractDataset, f::Vector{<:Function}, cols = :)
     colsidx = index(ds)[cols]
     @assert length(f) == length(colsidx) "number of provided functions must match the number of selected columns"
-    _hp_op_for_any_multi!(x, y) = x .= hp_bool_add_multi!(x, y)
+    _hp_op_for_any_multi!(x, y, f) = x .= hp_bool_add_multi!(x, y, f)
     # mapreduce(identity, op_for_anymissing!, eachcol(ds)[colsidx[sel_colsidx]], init = zeros(Bool, size(ds,1)))
     mapreduce_index(f, _hp_op_for_any_multi!, view(_columns(ds),colsidx), zeros(Bool, size(ds,1)))
 end
 
 _op_bool_mult_multi(x::Bool,y::Bool) = x && y ? true : false
 
-function hp_bool_mult_multi!(x, y)
+function hp_bool_mult_multi!(x, y, f)
     Threads.@threads for i in 1:length(x)
-        @inbounds x[i] = _op_bool_mult_multi(x[i], y[i])
+        @inbounds x[i] = _op_bool_mult_multi(x[i], f(y[i]))
     end
     x
 end
@@ -58,7 +58,7 @@ end
 function hp_row_all_multi(ds::AbstractDataset, f::Vector{<:Function}, cols = :)
     colsidx = index(ds)[cols]
     @assert length(f) == length(colsidx) "number of provided functions must match the number of selected columns"
-    _hp_op_for_all_multi!(x, y) = x .= hp_bool_mult_multi!(x, y)
+    _hp_op_for_all_multi!(x, y, f) = x .= hp_bool_mult_multi!(x, y, f)
     # mapreduce(identity, op_for_anymissing!, eachcol(ds)[colsidx[sel_colsidx]], init = zeros(Bool, size(ds,1)))
     mapreduce_index(f, _hp_op_for_all_multi!, view(_columns(ds),colsidx), ones(Bool, size(ds,1)))
 end
