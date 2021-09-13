722e8729c0a55bc9ad5c0544b6d52f76af9f6ea3
sl-solution <github.sl.solution@icloud.com>
2021-09-13 20:14:20 +1200

    Update byrow.jl

--- src/byrow/byrow.jl
+++ src/byrow/byrow.jl
@@ -7,19 +7,19 @@ stdze!(::_DUMMY_STRUCT) = false
 stdze(::_DUMMY_STRUCT) = false
 
 
-byrow(ds::AbstractDataset, ::typeof(sum), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = identity, threads = true) = threads ? hp_row_sum(ds, by, cols) : row_sum(ds, by, cols)
-byrow(ds::AbstractDataset, ::typeof(sum), col::ColumnIndex; by = identity, threads = true) = byrow(ds, sum, [col]; by = by, threads = threads)
+byrow(ds::AbstractDataset, ::typeof(sum), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = identity, threads = true && nrow(ds)>1000) = threads ? hp_row_sum(ds, by, cols) : row_sum(ds, by, cols)
+byrow(ds::AbstractDataset, ::typeof(sum), col::ColumnIndex; by = identity, threads = true && nrow(ds)>1000) = byrow(ds, sum, [col]; by = by, threads = threads)
 
-byrow(ds::AbstractDataset, ::typeof(prod), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = identity, threads = true) = threads ? hp_row_prod(ds, by, cols) : row_prod(ds, by, cols)
-byrow(ds::AbstractDataset, ::typeof(prod), col::ColumnIndex; by = identity, threads = true) = byrow(ds, prod, [col]; by = by, threads = threads)
+byrow(ds::AbstractDataset, ::typeof(prod), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = identity, threads = true && nrow(ds)>1000) = threads ? hp_row_prod(ds, by, cols) : row_prod(ds, by, cols)
+byrow(ds::AbstractDataset, ::typeof(prod), col::ColumnIndex; by = identity, threads = true && nrow(ds)>1000) = byrow(ds, prod, [col]; by = by, threads = threads)
 
 
-byrow(ds::AbstractDataset, ::typeof(count), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = isequal(true), threads = true) = threads ? hp_row_count(ds, by, cols) : row_count(ds, by, cols)
-byrow(ds::AbstractDataset, ::typeof(count), col::ColumnIndex; by = isequal(true), threads = true) = byrow(ds, count, [col], by = by, threads = threads)
+byrow(ds::AbstractDataset, ::typeof(count), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = isequal(true), threads = true && nrow(ds)>1000) = threads ? hp_row_count(ds, by, cols) : row_count(ds, by, cols)
+byrow(ds::AbstractDataset, ::typeof(count), col::ColumnIndex; by = isequal(true), threads = true && nrow(ds)>1000) = byrow(ds, count, [col], by = by, threads = threads)
 
 # byrow(ds::AbstractDataset, ::typeof(anymissing), cols::MultiColumnIndex = names(ds, Union{Missing, Number})) = row_anymissing(ds, cols)
 
-function byrow(ds::AbstractDataset, ::typeof(any), cols::MultiColumnIndex = :; by = x->isequal(true, x), threads = true)
+function byrow(ds::AbstractDataset, ::typeof(any), cols::MultiColumnIndex = :; by = x->isequal(true, x), threads = true && nrow(ds)>1000)
 	colsidx = index(ds)[cols]
 	if by isa AbstractVector
 		bys = map(_bool, by)
@@ -30,9 +30,9 @@ function byrow(ds::AbstractDataset, ::typeof(any), cols::MultiColumnIndex = :; b
 	end
 end
 
-byrow(ds::AbstractDataset, ::typeof(any), col::ColumnIndex; by = x->isequal(true, x), threads = true) = byrow(ds, any, [col]; by = by, threads = threads)
+byrow(ds::AbstractDataset, ::typeof(any), col::ColumnIndex; by = x->isequal(true, x), threads = true && nrow(ds)>1000) = byrow(ds, any, [col]; by = by, threads = threads)
 
-function byrow(ds::AbstractDataset, ::typeof(all), cols::MultiColumnIndex = :; by = x->isequal(true, x), threads = true)
+function byrow(ds::AbstractDataset, ::typeof(all), cols::MultiColumnIndex = :; by = x->isequal(true, x), threads = true && nrow(ds)>1000)
 	colsidx = index(ds)[cols]
 	if by isa AbstractVector
 		bys = map(_bool, by)
@@ -42,23 +42,23 @@ function byrow(ds::AbstractDataset, ::typeof(all), cols::MultiColumnIndex = :; b
 		threads ? hp_row_all_multi(ds, bys, colsidx) : row_all_multi(ds, bys, colsidx)
 	end
 end
-byrow(ds::AbstractDataset, ::typeof(all), col::ColumnIndex; by = x->isequal(true, x), threads = true) = byrow(ds, all, [col]; by = by, threads = threads)
+byrow(ds::AbstractDataset, ::typeof(all), col::ColumnIndex; by = x->isequal(true, x), threads = true && nrow(ds)>1000) = byrow(ds, all, [col]; by = by, threads = threads)
 
-byrow(ds::AbstractDataset, ::typeof(mean), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = identity, threads = true) = threads ? hp_row_mean(ds, by, cols) : row_mean(ds, by, cols)
-byrow(ds::AbstractDataset, ::typeof(mean), col::ColumnIndex; by = identity, threads = true) = byrow(ds, mean, [col]; by = by, threads = threads)
+byrow(ds::AbstractDataset, ::typeof(mean), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = identity, threads = true && nrow(ds)>1000) = threads ? hp_row_mean(ds, by, cols) : row_mean(ds, by, cols)
+byrow(ds::AbstractDataset, ::typeof(mean), col::ColumnIndex; by = identity, threads = true && nrow(ds)>1000) = byrow(ds, mean, [col]; by = by, threads = threads)
 
 
-byrow(ds::AbstractDataset, ::typeof(maximum), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = identity, threads = true) = threads ? hp_row_maximum(ds, by, cols) : row_maximum(ds, by, cols)
-byrow(ds::AbstractDataset, ::typeof(maximum), col::ColumnIndex; by = identity, threads = true) = byrow(ds, maximum, [col]; by = by, threads = threads)
+byrow(ds::AbstractDataset, ::typeof(maximum), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = identity, threads = true && nrow(ds)>1000) = threads ? hp_row_maximum(ds, by, cols) : row_maximum(ds, by, cols)
+byrow(ds::AbstractDataset, ::typeof(maximum), col::ColumnIndex; by = identity, threads = true && nrow(ds)>1000) = byrow(ds, maximum, [col]; by = by, threads = threads)
 
-byrow(ds::AbstractDataset, ::typeof(minimum), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = identity, threads = true) = threads ? hp_row_minimum(ds, by, cols) : row_minimum(ds, by, cols)
-byrow(ds::AbstractDataset, ::typeof(minimum), col::ColumnIndex; by = identity, threads = true) = byrow(ds, minimum, [col]; by = by, threads = threads)
+byrow(ds::AbstractDataset, ::typeof(minimum), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = identity, threads = true && nrow(ds)>1000) = threads ? hp_row_minimum(ds, by, cols) : row_minimum(ds, by, cols)
+byrow(ds::AbstractDataset, ::typeof(minimum), col::ColumnIndex; by = identity, threads = true && nrow(ds)>1000) = byrow(ds, minimum, [col]; by = by, threads = threads)
 
-byrow(ds::AbstractDataset, ::typeof(var), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = identity, dof = true, threads = true) = threads ? hp_row_var(ds, by, cols; dof = dof) : row_var(ds, by, cols; dof = dof)
-byrow(ds::AbstractDataset, ::typeof(var), col::ColumnIndex; by = identity, dof = true, threads = true) = byrow(ds, var, [col]; by = by, dof = dof, threads = threads)
+byrow(ds::AbstractDataset, ::typeof(var), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = identity, dof = true, threads = true && nrow(ds)>1000) = threads ? hp_row_var(ds, by, cols; dof = dof) : row_var(ds, by, cols; dof = dof)
+byrow(ds::AbstractDataset, ::typeof(var), col::ColumnIndex; by = identity, dof = true, threads = true && nrow(ds)>1000) = byrow(ds, var, [col]; by = by, dof = dof, threads = threads)
 
-byrow(ds::AbstractDataset, ::typeof(std), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = identity, dof = true, threads = true) = threads ? hp_row_std(ds, by, cols; dof = dof) : row_std(ds, by, cols; dof = dof)
-byrow(ds::AbstractDataset, ::typeof(std), col::ColumnIndex; by = identity, dof = true, threads = true) = byrow(ds, std, [col]; by = by, dof = dof, threads = threads)
+byrow(ds::AbstractDataset, ::typeof(std), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = identity, dof = true, threads = true && nrow(ds)>1000) = threads ? hp_row_std(ds, by, cols; dof = dof) : row_std(ds, by, cols; dof = dof)
+byrow(ds::AbstractDataset, ::typeof(std), col::ColumnIndex; by = identity, dof = true, threads = true && nrow(ds)>1000) = byrow(ds, std, [col]; by = by, dof = dof, threads = threads)
 
 byrow(ds::AbstractDataset, ::typeof(nunique), cols::MultiColumnIndex = names(ds, Union{Missing, Number}); by = identity, count_missing = true) = row_nunique(ds, by, cols; count_missing = count_missing)
 byrow(ds::AbstractDataset, ::typeof(nunique), col::ColumnIndex; by = identity, count_missing = true) = byrow(ds, nunique, [col]; by = by, count_missing = count_missing)
@@ -85,19 +85,19 @@ byrow(ds::AbstractDataset, ::typeof(stdze), cols::MultiColumnIndex = names(ds, U
 
 byrow(ds::AbstractDataset, ::typeof(stdze!), cols::MultiColumnIndex = names(ds, Union{Missing, Number})) = row_stdze!(ds, cols)
 
-byrow(ds::AbstractDataset, ::typeof(hash), cols::MultiColumnIndex = :; by = identity, threads = true) = threads ? row_hash_hp(ds, by, cols) : row_hash(ds, by, cols)
-byrow(ds::AbstractDataset, ::typeof(hash), col::ColumnIndex; by = identity, threads = true) = byrow(ds, hash, [col]; by = by, threads = threads)
+byrow(ds::AbstractDataset, ::typeof(hash), cols::MultiColumnIndex = :; by = identity, threads = true && nrow(ds)>1000) = threads ? row_hash_hp(ds, by, cols) : row_hash(ds, by, cols)
+byrow(ds::AbstractDataset, ::typeof(hash), col::ColumnIndex; by = identity, threads = true && nrow(ds)>1000) = byrow(ds, hash, [col]; by = by, threads = threads)
 
 byrow(ds::AbstractDataset, ::typeof(mapreduce), cols::Union{MultiColumnIndex, ColumnIndex} = names(ds, Union{Missing, Number}); op = .+, kwargs...) = mapreduce(identity, op, eachcol(ds[!, cols]); kwargs...)
 
 byrow(ds::AbstractDataset, ::typeof(reduce), cols::Union{MultiColumnIndex, ColumnIndex} = names(ds, Union{Missing, Number}); op = .+, kwargs...) = reduce(op, eachcol(ds[!, cols]); kwargs...)
 
-function byrow(ds::AbstractDataset, f::Function, cols::MultiColumnIndex; threads = true)
+function byrow(ds::AbstractDataset, f::Function, cols::MultiColumnIndex; threads = true && nrow(ds)>1000)
 	colsidx = index(ds)[cols]
 	length(colsidx) == 1 && return byrow(ds, f, colsidx[1]; threads = threads)
 	threads ?  hp_row_generic(ds, f, cols) : row_generic(ds, f, cols)
 end
-function byrow(ds::AbstractDataset, f::Function, col::ColumnIndex; threads = true)
+function byrow(ds::AbstractDataset, f::Function, col::ColumnIndex; threads = true && nrow(ds)>1000)
 	if threads
 		T = Core.Compiler.return_type(f, (nonmissingtype(eltype(ds[!, col])), ))
 		res = Vector{Union{Missing, T}}(undef, nrow(ds))
