811747c55dd94ca487c4d1ffba100cc26ebce6a1
sl-solution <github.sl.solution@icloud.com>
2021-09-14 21:39:26 +1200

    Revert "add byrow and leftjoin"
    This reverts commit 9f46a0bf00c2a926b2deb20723aadb6f9423542d.

--- src/precompile/warmup.jl
+++ src/precompile/warmup.jl
@@ -1,7 +1,7 @@
 function warmup()
     t1 = now()
     ds = Dataset(x1 = rand(1:2, 1000), x2 = rand(1:2, 1000), x3 = rand(1:2, 1000) * 1.1, x4 = rand([Date(1), Date(2)], 1000), x5 = string.(rand(1:2), 1000),
-                 x6 = PooledArray(Characters{8, UInt8}.(rand(1:2, 1000))), x7 = Int32.(rand(1:2, 1000)), x8 = Float32.(rand(1:2, 1000) * 1.1))
+                 x6 = PooledArray(Characters{2, UInt8}.(rand(1:2, 1000))), x7 = Int32.(rand(1:2, 1000)), x8 = Float32.(rand(1:2, 1000) * 1.1))
     for i in 1:8
         sortperm(ds, i)
     end
@@ -20,14 +20,11 @@ function warmup()
     for i in 1:8
         groupby(ds, 1:i)
     end
-    byrow(ds, all, :, by = isequal(1))
-    byrow(ds, sum)
     combine(groupby(ds,1), Ref([1,2,3,7,8]) .=> [sum, mean, length, maximum, minimum, var, std, median, sort])
     ds2 = ds[1:2, [1,3]]
-    leftjoin(ds, ds2, on = [:x1, :x3])
     innerjoin(ds, ds2, on = [:x1, :x3])
     transpose(ds, 1:8)
     transpose(groupby(ds,1), [2,3])
     t2 = now()
-    Dataset(x1 = "Finish warmup in", x2 = t2-t1)
+    Dataset(x1 = "Time to finish warmup", x2 = t2-t1)
 end
