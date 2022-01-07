c0d82c9b09430bfba14b280f82563c02346edca9
sl-solution <github.sl.solution@icloud.com>
2022-01-07 23:00:32 +1300

    option to add DLMReader to sysimage

--- src/precompile/create_sysimage.jl
+++ src/precompile/create_sysimage.jl
@@ -1,10 +1,21 @@
-function create_sysimage(sysout::AbstractString)
+function create_sysimage(sysout::AbstractString, add_dlmreader::Bool = true; add_package::Bool = true)
     juliaCMD = Base.julia_cmd()
     tmpIMD = mktemp()
     close(tmpIMD[2])
-    run(`$juliaCMD -e "using Pkg; Pkg.add(\"PackageCompiler\")"`)
+    if add_package
+        run(`$juliaCMD -e "using Pkg; Pkg.add(\"PackageCompiler\")"`)
+        if add_dlmreader
+            run(`$juliaCMD -e "using Pkg; Pkg.add(\"DLMReader\")"`)
+        end
+    end
+    warmupcode = "using InMemoryDatasets; ds = IMD.warmup();println(ds)"
+    if add_dlmreader
+        tmpDLM = mktemp()
+        close(tmpDLM[2])
+        warmupcode *= "; using DLMReader; repeat!(ds, 10000); ds.x1 = rand(10000); ds.x2 = rand(Int, 10000); filewriter(\"$(tmpDLM[1])\", ds); filereader(\"$(tmpDLM[1])\");"
+    end
 
-    run(`$juliaCMD --trace-compile=$(tmpIMD[1]) -e "using InMemoryDatasets; ds = IMD.warmup();println(ds)"`)
+    run(`$juliaCMD --trace-compile=$(tmpIMD[1]) -e "$warmupcode"`)
     tmpIMD_out = mktemp()
     close(tmpIMD_out[2])
     f = open(tmpIMD[1])
@@ -12,6 +23,10 @@ function create_sysimage(sysout::AbstractString)
     # TODO should make this part less hard coding
     write(fout, "using REPL, Pkg, InMemoryDatasets, InMemoryDatasets.PooledArrays, InMemoryDatasets.DataAPI, InMemoryDatasets.PrettyTables, InMemoryDatasets.Tables, PackageCompiler, TOML, Logging, SuiteSparse
     ")
+    if add_dlmreader
+        write(fout, "; using DLMReader, DLMReader.InlineStrings.Parsers
+        ")
+    end
     for line in eachline(f)
         # not sure why this happens
         write(fout, replace(line, "000000"=>""))
